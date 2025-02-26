704141957fb0800b48becdb8fcb78548f8df3247
Rahul Bhoir <rahulbhoir767676@gmail.com>
2025-02-26 16:06:52 +0530

    up

--- src/pages/Navbar.js
+++ src/pages/Navbar.js
@@ -1,12 +1,17 @@
 import React from 'react'
+import logo from "../images/TravelGo.png"
 
 const Navbar = () => {
   return (
-    <div>
-      <li>Home</li>
-      <li>Tours</li>
-      <li>About</li>
-      <li>Contact Us</li>
+    <div className='navbar'>
+      <img src={logo} width="100px" height="100px" alt='' />
+      <ul>
+        <li>Home</li>
+        <li>Tours</li>
+        <li>About</li>
+        <li>Contact Us</li>
+      </ul>
+      <button className='loginbtn'>Login</button>
     </div>
   )
 }
