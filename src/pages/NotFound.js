35330115c88116831c6916823a3e42dde1ee0e8e
Rahul Bhoir <rahulbhoir767676@gmail.com>
2025-03-03 20:29:00 +0530

    up

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,10 +1,28 @@
 import React from 'react'
+import Skull from "../images/skull.png"
+import { useNavigate } from 'react-router-dom'
 
 const NotFound = () => {
+  const navigate = useNavigate()
   return (
+    <>
     <div>
-      404 | Page not found
+    <img className='skull' src={Skull}/>
+    <div>404 | Page not found </div>
+    <button 
+      onClick={() => navigate("/")} 
+      style={{
+        marginLeft: "35%",
+        border: "none",
+        backgroundColor: "black",
+        color: "white",
+        width: "80px",
+        height: "50px",
+        borderRadius: "10px"
+      }}   
+    >Home</button>
     </div>
+    </>
   )
 }
 
